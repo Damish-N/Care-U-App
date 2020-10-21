501b1692f74afff4ecfe9aef73e184d372d0e6ab
Damish <Damishnisal100@gmail.com>
2020-10-22 01:09:37 +0530

    Revert "edit-FirstAid_instructions"
    This reverts commit 7c402695

--- app/src/main/java/com/example/careu/Pop1.java
+++ app/src/main/java/com/example/careu/Pop1.java
@@ -2,13 +2,7 @@ package com.example.careu;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.DisplayMetrics;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-
-import org.w3c.dom.Text;
 
 
 public class Pop1 extends Activity {
@@ -19,7 +13,6 @@ public class Pop1 extends Activity {
 
         setContentView(R.layout.pop1_window);
 
-
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
@@ -28,14 +21,4 @@ public class Pop1 extends Activity {
 
         getWindow().setLayout((int)(width*.8),(int)(height*.7));
     }
-
-    /* @Override
-        public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-            super.onCreate(savedInstanceState, persistentState);
-            setContentView(R.layout.pop1_window);
-
-            Text view txt = (TextView) findViewById(R.id.t1);
-            txt.setMovement
-        }*/
-
 }
